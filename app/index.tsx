475cb98d5453a1887cad6b6ba9b5020842ba3932
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-05-24 08:33:44 +0530

    almost done project setup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      {/* <StatusBar style="auto" /> */}
+      <StatusBar />
+      <Link href="/profile">Go to Profile</Link>
+    </View>
+  );
+}
